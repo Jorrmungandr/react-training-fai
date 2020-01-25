16305e6772e76a04bfa94bdfdf9bf18ae70a056f
Edgar Marques <edgarmarques.110@gmail.com>
2020-01-25 11:39:52 -0300

    Add base of project

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <section className="app">
+      Hello World
+    </section>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
